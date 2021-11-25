538f343b0987d61ae9bad2d2c5bfbec1a701aa70
Arsh Meharwal <arshmeharwal98@gmail.com>
2021-11-25 22:31:14 +0530

    Create 01.Singly Linked List Starter Code.js

---
+++ 01.Singly Linked List Starter Code.js
@@ -0,0 +1,10 @@
+class Node{
+    constructor(val){
+        this.val = val;
+        this.next = null; 
+    }
+}
+
+var first= new Node ("arsh");
+first.next = new Node("is a winner");
+first.next.next = new Node("will be a winner")
