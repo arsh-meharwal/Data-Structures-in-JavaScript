079551fcd501922eeb26b0152b90db933d12d7a2
Arsh Meharwal <arshmeharwal98@gmail.com>
2021-11-29 22:09:08 +0530

    Create 07.Get in SLL.js

---
+++ 07.Get in SLL.js
@@ -0,0 +1,101 @@
+class Node{
+    constructor(val){
+        this.val = val;
+        this.next = null; 
+    }
+}
+
+class SinglyLinkedList{
+    constructor(){
+        this.head = null;
+        this.tail = null;
+        this.length = null;
+    }
+    traverse(){
+        var currentVal = this.head
+        while(currentVal){
+            console.log(currentVal.val);
+            currentVal = currentVal.next;
+        }
+    }
+
+    push(val){
+        var newNode = new Node(val);
+        if(!this.head){
+            this.head = newNode;
+            this.tail = newNode;
+        }
+        else{
+            this.tail.next = newNode;
+            this.tail = newNode;
+        }
+        this.length++;
+        return this;
+
+    }
+    pop(){
+        let current = this.head;
+        let newtail = current;
+
+        if(!this.head){
+            return undefined;
+        }
+        else{
+            while(current.next){
+                newtail = current;
+                current = current.next; 
+            }
+
+        }
+        this.tail = newtail;
+        this.tail.next = null;
+        this.length--;
+        if(this.length === 0){
+            this.head = null;
+            this.tail = null;
+        }
+        return current;
+        
+    }
+    shift(){
+        if(!this.head) return undefined;
+         var currentHead= this.head ;
+         var newHead = this.head.next;
+         this.head = newHead;
+         this.length--;
+         return currentHead;
+
+    }
+    unshift(val){
+        var newNode = new Node(val);
+        if(!this.head){
+            this.head = newNode;
+            this.tail = newNode;
+        }
+        else{
+            var temp = this.head;
+            this.head = newNode;
+            this.head.next = temp;
+            console.log(temp.val);
+        }
+        this.length++;
+        return this;
+         
+    }
+    get(index){
+        if(index<0||index>this.length) return null;  
+        var current = this.head;
+        for(i=0;i<index;i++){
+            current = current.next
+        } 
+        return current;
+    }
+}
+
+
+var jist = new SinglyLinkedList()
+jist.push("JHI");
+jist.push("HI");
+jist.push("I am arsh");
+jist.push("How are you");
+jist.push("I am fine");
