80eb14feb534b15ad85218e6a5b33d4c42b2e607
Arsh Meharwal <arshmeharwal98@gmail.com>
2021-12-03 14:52:00 +0530

    Create 18. Get in DLL.js

---
+++ 18. Get in DLL.js
@@ -0,0 +1,90 @@
+class Node{
+    constructor(val){
+        this.val = val;
+        this.next = null;
+        this.prev = null;
+    }
+}
+
+class doublyLinkedList{
+    constructor(){
+        this.head = null;
+        this.tail = null;
+        this.length = 0;
+    }
+
+    push(val){
+        var newNode = new Node(val);
+        if(!this.head){
+            this.head = newNode;
+            this.tail = newNode;
+        }else{
+            this.tail.next = newNode;
+            newNode.prev = this.tail;
+            this.tail = newNode;
+            
+        }
+        this.length++;
+        return this;
+    }
+
+    pop(){
+        var newTail = this.tail.prev;
+        if(!this.head) return undefined;
+        if(this.length === 1){
+            this.head = null;
+            this.tail = null;
+        }
+        this.tail = newTail;
+        this.tail.next = null;
+        this.length--;
+        return this;
+    }
+    shift(){
+        if(!this.head){
+            return false;
+        }
+        if(this.length===1){
+            this.head = null;
+            this.tail = null;
+        }else{
+            var oldHead = this.head;
+            var newHead = this.head.next;
+            this.head = newHead;
+            this.head.prev = null;
+            this.length--;
+            return this;
+        }
+
+    }
+    // unshift means adding a new head
+    unshift(val){
+        var newNode = new Node(val);
+        if(!this.head){
+            this.head = newNode;
+            this.tail = newNode;
+        }else{
+            var oldHead = this.head;
+            this.head = newNode;
+            this.head.next = oldHead;
+            oldHead.prev = newNode;
+            this.length++;
+            return this;
+        }
+    }
+    get(index){
+        if(index<0||index>this.length) return null;  
+        var current = this.head;
+        for(i=0;i<index;i++){
+            current = current.next
+        } 
+        return current;
+    }
+    
+}
+
+var list = new doublyLinkedList();
+list.push("hi");
+list.push("I am");
+list.push("arsh");
+list.push("meharwal")
