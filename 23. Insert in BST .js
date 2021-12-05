ec9fbf7b8fc80fe1feb42a5ecef431ee0ec077c3
Arsh Meharwal <arshmeharwal98@gmail.com>
2021-12-05 14:44:15 +0530

    Create 23. Insert in BST .js

---
+++ 23. Insert in BST .js
@@ -0,0 +1,47 @@
+class Node {
+    constructor(value){
+        this.value = value;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+class BinarySearchTree {
+    constructor(){
+        this.root = null;
+    }
+    insert(value){
+        var newNode = new Node(value);
+        if(this.root === null){
+            this.root = newNode;
+            return this;
+        }
+        var current = this.root;
+        while(true){
+            if(value === current.value) return undefined;
+            if(value < current.value){
+                if(current.left === null){
+                    current.left = newNode;
+                    return this;
+                }
+                current = current.left;
+            } else {
+                if(current.right === null){
+                    current.right = newNode;
+                    return this;
+                } 
+                current = current.right;
+            }
+        }
+    }
+}
+
+
+var tree = new BinarySearchTree();
+tree.insert(100);
+tree.insert(80);
+tree.insert(1);
+tree.insert(20);
+tree.insert(31);
+tree.insert(105);
+tree.insert(400);
